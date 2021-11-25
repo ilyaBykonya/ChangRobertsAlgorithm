c985bbf40a060998e8a5d4359d317c4c538e7d96
ilyaBykonya <illya.aleksandrowich@mail.ru>
2021-11-25 18:11:00 +0300

    2.0.0

--- main.cpp
+++ main.cpp
@@ -2,11 +2,5 @@
 
 int main()
 {
-    //Здесь мы можем указать путь к любому файлу с тестовой конфигурацией и файлу, в который будет записан ответ
-    string configFileName = "D:/Work/Freelance/ChangRobertsAlgorithm/test_1.txt";
-    string outputFileName = "D:/Work/Freelance/ChangRobertsAlgorithm/result_1.txt";
-
-    uint16_t result_leader = start_test(configFileName);
-    ofstream outFile(outputFileName);
-    outFile << "Leader has uid: " << result_leader;
+    return 0;
 }
